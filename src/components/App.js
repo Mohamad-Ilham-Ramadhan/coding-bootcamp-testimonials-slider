d1b2c8a6d07ff27760251894b12ad1c4dcf4b7e6
Mohamad Ilham Ramadhan <ilhamzoldyck13@gmail.com>
2020-06-24 06:59:04 +0700

    done mobile and desktop design

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import bgPattern from "../images/pattern-bg.svg";
+import bgQuotes from "../images/pattern-quotes.svg";
+import authorFigure from "../images/image-john.jpg";
+import iconNext from "../images/icon-next.svg";
+import iconPrev from "../images/icon-prev.svg";
+import { fakeDatabase as database } from "../fakeAPI";
 
 export default function App() {
+  const [users, setUsers] = useState(null);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {}, []);
   return (
     <section className="container">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste consectetur
-      aliquid dolorem vero. Harum quod nisi quae, consequatur incidunt velit
-      saepe earum impedit amet totam debitis iure nostrum, quasi sequi?
-      <div className="bg bg--curve"></div>
       <article className="slider">
-        <div className="slider__item">
-          <img className="slider__bg slider__bg--pattern" src={bgPattern} />
-          <q className="slider__quotes"></q>
-          <p className="slider__author"></p>
-        </div>
-        <div className="slider__item">
-          <img className="slider__bg slider__bg--quotes" />
-          <figure className="slider__figure">
-            <img src="" alt="" />
-          </figure>
-          <div className="slider__controls">
-            <button className="slider__control__prev"></button>
-            <button className="slider__control__next"></button>
+        <div className="slider__item slider__item--quotes">
+          <div className="slider__quotes">
+            <img className="slider__bg slider__bg--quotes" src={bgQuotes} />
+            <q className="slider__quotes__text">
+              If you want to lay the best foundation possible I'd recommend
+              taking this course. The depth the instructors go into is
+              incredible. I now feel so confident about starting up as a
+              professional developer.
+            </q>
+            <p className="slider__quotes__author">
+              <span className="slider__quotes__author__name">John Tarkpor</span>
+              <span className="slider__quotes__author__occupation">
+                Junior Front-end Developer
+              </span>
+            </p>
           </div>
+        </div>
+
+        <div className="slider__item slider__item--figure">
+          <figure className="slider__figure">
+            <img className="slider__bg slider__bg--figure" src={bgPattern} />
+            <div className="wrapper --relative">
+              <img
+                className="slider__figure__img"
+                src={authorFigure}
+                alt="author figure"
+              />
+              <div className="slider__controls">
+                <button className="slider__controls__btn slider__controls__btn--prev">
+                  <img src={iconPrev} alt="" />
+                </button>
+                <button className="slider__controls__btn slider__controls__btn--next">
+                  <img src={iconNext} alt="" />
+                </button>
+              </div>
+            </div>
+          </figure>
         </div>
       </article>
     </section>
---
+++ src/fakeAPI.js
@@ -0,0 +1,25 @@
+import { v4 as uuid } from "uuid";
+import imageJohn from "./images/image-john.jpg";
+import imageTanya from "./images/image-tanya.jpg";
+const fakeDatabase = {
+  users: [
+    {
+      id: uuid(),
+      image: imageJohn,
+      fullname: "John Tarkpor",
+      occupation: "Junior Front-end Developer",
+      testimonial:
+        "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+    },
+    {
+      id: uuid(),
+      image: imageTanya,
+      fullname: "Tanya Sinclair",
+      occupation: "UX Engineer",
+      testimonial:
+        "I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so exited about the future.",
+    },
+  ],
+};
+
+export { fakeDatabase };
